f9bda4464c03a526297389185f8875e5c15a1170
zpsong-tower <pingzisong2012@gmail.com>
2022-11-18 02:42:21 +0800

    temp

--- LeetCode/src/pers/tower/Test.kt
+++ LeetCode/src/pers/tower/Test.kt
@@ -1,18 +1,18 @@
-package pers.tower;
+package pers.tower
 
-import pers.tower.questions.*;
+import pers.tower.questions.*
 
 /**
  * 测试类，测试当前题目输出是否符合预期
  *
- * @author zpsong-tower <pingzisong2012@gmail.com>
+ * @author zpsong-tower <pingzisong2012></pingzisong2012>@gmail.com>
  * @since 2020/11/6 3:01
  */
-public class Test {
-	public static void main(String[] str) {
-		Question316 q = new Question316();
-		System.out.println(q.removeDuplicateLetters("bcabc")); // abc
-		System.out.println(q.removeDuplicateLetters("cbacdcbc")); // acdb
-	}
-
+object Test {
+    @JvmStatic
+    fun main(str: Array<String>) {
+        val q = Question316()
+        println(q.removeDuplicateLetters("bcabc")) // abc
+        println(q.removeDuplicateLetters("cbacdcbc")) // acdb
+    }
 }
